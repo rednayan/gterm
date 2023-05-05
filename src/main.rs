f74627c760dcf7c4d707298f70055cbc06f94bfc
rednayan <nayansharma1010@gmail.com>
2023-05-05 18:00:29 +0530

    print latest commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use git2::{Commit, ObjectType, Repository};
+
 fn main() {
-    println!("Hello, world!");
+    let repo = match Repository::init("/home/syien/rust/gterm") {
+        Ok(repo) => repo,
+        Err(e) => panic!("ERROR loading repository: {e}"),
+    };
+    let obj = repo
+        .head()
+        .unwrap()
+        .resolve()
+        .unwrap()
+        .peel(ObjectType::Commit)
+        .unwrap();
+    let commit = obj.clone().into_commit().unwrap();
+    let timestamp = commit.time().seconds();
+    println!(
+        "{commit_id}\n{commit_author}\n{timestamp}\n{commit_message}",
+        commit_id = commit.id(),
+        commit_author = commit.author(),
+        commit_message = commit.message().unwrap()
+    );
 }
