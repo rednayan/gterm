1e434c0912fd0a388fddd3bac0cd4f1e85752fcf
rednayan <nayansharma1010@gmail.com>
2023-05-06 12:06:12 +0530

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,23 @@ fn main() {
         Ok(repo) => repo,
         Err(e) => panic!("ERROR loading repository: {e}"),
     };
-    let obj = repo
-        .head()
-        .unwrap()
-        .resolve()
-        .unwrap()
-        .peel(ObjectType::Commit)
-        .unwrap();
-    let commit = obj.clone().into_commit().unwrap();
+
+    display_last_commit(&repo);
+}
+
+fn find_last_commit(repo: &Repository) -> Result<Commit, git2::Error> {
+    let obj = repo.head()?.resolve()?.peel(ObjectType::Commit)?;
+    let commit = obj.into_commit().unwrap();
+    return Ok(commit);
+}
+
+fn display_last_commit(repo: &Repository) -> Result<(), git2::Error> {
+    let commit = find_last_commit(repo).expect("ERROR:Could not find last commit");
     let timestamp = commit.time().seconds();
-    println!(
-        "{commit_id}\n{commit_author}\n{timestamp}\n{commit_message}",
+    Ok(println!(
+        "commit_id: {commit_id}\ncommit_author: {commit_author}\ntimestamp: {timestamp}\ncommit_message: {commit_message}",
         commit_id = commit.id(),
         commit_author = commit.author(),
         commit_message = commit.message().unwrap()
-    );
+    ))
 }
