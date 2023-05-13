2044afceaf7386f4b20677fbfbf760a30e476cf6
rednayan <nayansharma1010@gmail.com>
2023-05-13 12:22:11 +0530

    new ui

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+use anyhow::Result;
 use crossterm::{
     event::{DisableMouseCapture, EnableMouseCapture},
     execute,
-    terminal::{EnterAlternateScreen, LeaveAlternateScreen},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use git2::{Commit, ObjectType, Repository};
-use std::env;
-use std::{io, thread, time};
-use tui::{
-    backend::CrosstermBackend,
-    widgets::{Block, Borders},
-    Terminal,
-};
+use git2::{Commit, Repository};
+use std::{env, io};
+use tui::{backend::CrosstermBackend, Terminal};
+mod ui;
 
-fn main() {
+fn main() -> Result<()> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
     let mut args = env::args();
     args.next();
     let path = args.next().expect("ERROR: please enter path to a folder");
@@ -20,7 +24,20 @@ fn main() {
         Ok(repo) => repo,
         Err(e) => panic!("ERROR loading repository: {e}"),
     };
+    commit_logs(&repo);
 
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
+}
+
+fn commit_logs(repo: &Repository) {
     let mut revwalk = repo.revwalk().unwrap();
     revwalk.push_head().unwrap();
 
@@ -29,27 +46,18 @@ fn main() {
         let commit = repo.find_commit(commit_oid).unwrap();
         display_commits(&commit).unwrap();
     }
-
-    let backend = CrosstermBackend::new(io::stdout());
-    execute!(io::stdout(), EnterAlternateScreen, EnableMouseCapture).unwrap();
-    let mut terminal = Terminal::new(backend).unwrap();
-    terminal
-        .draw(|f| {
-            let size = f.size();
-            let block = Block::default().title("Block").borders(Borders::ALL);
-            f.render_widget(block, size);
-        })
-        .unwrap();
-    thread::sleep(time::Duration::from_millis(5000));
-    execute!(io::stdout(), LeaveAlternateScreen, DisableMouseCapture).unwrap();
 }
 
 fn display_commits(commit: &Commit) -> Result<(), git2::Error> {
     let timestamp = commit.time().seconds();
     Ok(println!(
-        "commit_id: {commit_id}\ncommit_author: {commit_author}\ntimestamp: {timestamp}\ncommit_message: {commit_message}",
+        "commit_id: {commit_id}
+        \ncommit_author: {commit_author}
+        \ntimestamp: {timestamp}
+        \ncommit_message: {commit_message}
+       ",
         commit_id = commit.id(),
         commit_author = commit.author(),
-        commit_message = commit.message().unwrap()
+        commit_message = commit.message().unwrap(),
     ))
 }
